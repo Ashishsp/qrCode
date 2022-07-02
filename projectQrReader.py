97df56932b16878872135a4d146f69ef7a1188d7
manideep <74915635+Ashishsp@users.noreply.github.com>
2022-07-02 13:39:25 +0530

    Update projectQrReader.py

--- projectQrReader.py
+++ projectQrReader.py
@@ -1,9 +1,9 @@
-import pyqrcode
-from pyzbar.pyzbar import decode
-from PIL import Image
+import pyqrcode         #To generate qr code
+from pyzbar.pyzbar import decode          #To decode qrcode
+from PIL import Image         #To save in png formats
 
-qr = pyqrcode.create("This is what converted into QR code.")
-qr.png("qrCode.png", scale=16)
+qr = pyqrcode.create("This is what converted into QR code.")          #Creating qr code
+qr.png("qrCode.png", scale=16)          #Saving qr code in .png format. Scale refers to the scale of the png. Higher the scale, larger the image looks.
 
-d = decode(Image.open("qrCode.png"))
-print(d[0].data.decode("ascii"))
+d = decode(Image.open("qrCode.png"))          #Opening the .png image and decoding the qr code
+print(d[0].data.decode("ascii"))          #Displaying qr code in ascii format by decoding the binary information found in .png
